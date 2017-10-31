e2c5493d338f4351ab0b90a2e181e6937f5ac026
Cheranga Hatangala <cheranga@gmail.com>
2017-11-01 03:42:45 +1100

    Add project files.

---
+++ DI_Mvc_Autofac/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DI_Mvc_Autofac.MvcApplication" Language="C#" %>
---
+++ DI_Mvc_Autofac/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DI_Mvc_Autofac.Startup))]
+namespace DI_Mvc_Autofac
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
